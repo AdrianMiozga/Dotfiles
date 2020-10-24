83ca18ce20c0cc201a6f499d9dacfd327d31237f
AdrianMiozga <AdrianMiozga@outlook.com>
2020-10-24 03:18:16 +0200

    Add Select Item.lua

---
+++ Reaper Scripts/Select Item.lua
@@ -0,0 +1,16 @@
+-- If there are no items on current track go to beginning of the project.
+-- If there are items on current track, select and move to item on the
+-- left from cursor, or if it doesn't exist, item on the right side.
+
+if (reaper.CountTrackMediaItems(reaper.GetSelectedTrack(0, 0)) > 0) then
+    -- Select and move to previous item
+    reaper.Main_OnCommand(40416, 0)
+
+    if (reaper.CountSelectedMediaItems(0) == 0) then
+        -- Select and move to next item
+        reaper.Main_OnCommand(40417, 0)
+    end
+else
+    -- Go to beginning of project
+    reaper.Main_OnCommand(40042, 0)
+end
