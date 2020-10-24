c7957a9520be199cceb31c08615f5ec68e40cb20
AdrianMiozga <AdrianMiozga@outlook.com>
2020-10-24 02:43:01 +0200

    Add scripts to change project BPM

---
+++ Reaper Scripts/Decrease 5 BPM.lua
@@ -0,0 +1,7 @@
+-- Decrease project BPM by 5
+
+if (reaper.GetToggleCommandState(40259) == 0) then
+    reaper.Main_OnCommand(40259, 0)
+end
+
+reaper.SetCurrentBPM(0, reaper.TimeMap2_GetDividedBpmAtTime(0, 0) - 5, true)
