b0394859fcd43dd36f02bbb18ccaa9096c2c703e
AdrianMiozga <AdrianMiozga@outlook.com>
2020-11-16 07:07:24 +0100

    Show only the first level of to-do items in agenda

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -95,6 +95,7 @@
   (setq org-agenda-start-on-weekday 1)
   (setq calendar-week-start-day 1)
   (setq org-agenda-weekend-days 'nil)
+  (setq org-agenda-todo-list-sublevels 'nil)
   (setq org-agenda-files '("~/org-roam/20200820212859-main.org"))
   (setq org-agenda-prefix-format '((agenda . " %i %?-12t% s")
                                    (todo . " %i ")
