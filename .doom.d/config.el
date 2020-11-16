36fdb335076e17d54851164bc24d3b7a32841f22
AdrianMiozga <AdrianMiozga@outlook.com>
2020-11-16 07:06:27 +0100

    Stop highlighting weekend days in agenda

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -94,6 +94,7 @@
   (map! :prefix "C-c" "a" #'(lambda (&optional arg) (interactive "P")(org-agenda arg "c")))
   (setq org-agenda-start-on-weekday 1)
   (setq calendar-week-start-day 1)
+  (setq org-agenda-weekend-days 'nil)
   (setq org-agenda-files '("~/org-roam/20200820212859-main.org"))
   (setq org-agenda-prefix-format '((agenda . " %i %?-12t% s")
                                    (todo . " %i ")
