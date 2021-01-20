cc5f26c5867343acdfd39d78d75d3fe2b28a9693
Adrian Miozga <AdrianMiozga@outlook.com>
2021-01-20 10:04:05 +0100

    Add lua support

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -148,8 +148,8 @@
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
-       ;;lua               ; one-based indices? one-based indices
        ;;markdown          ; writing docs for people to ignore
+       lua                 ; one-based indices? one-based indices
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
