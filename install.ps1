1bb592cdd0b70e2bc3cf62f97ec1339adfb39c96
Adrian Miozga <AdrianMiozga@outlook.com>
2023-11-23 00:43:43 +0100

    Add date snippet

--- install.ps1
+++ install.ps1
@@ -21,5 +21,6 @@ New-Item -Force -Path "C:\Program Files\Dual Key Remap\config.txt" -ItemType Sym
 New-Item -Force -Path C:\Users\Adrian\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json -ItemType SymbolicLink -Value $HOME\dotfiles\windows-terminal-settings.json
 New-Item -Force -Path $HOME\gallery-dl.conf -ItemType SymbolicLink -Value $HOME\dotfiles\gallery-dl.conf
 New-Item -Force -Path $HOME\yt-dlp.conf -ItemType SymbolicLink -Value $HOME\dotfiles\yt-dlp.conf
+New-Item -Force -Path $HOME\AppData\Roaming\Code\User\snippets\ -ItemType SymbolicLink -Value "$HOME\dotfiles\VS Code\Snippets"
 New-Item -Force -Path $HOME\AppData\Roaming\Code\User\keybindings.json -ItemType SymbolicLink -Value "$HOME\dotfiles\VS Code\keybindings.json"
 New-Item -Force -Path $HOME\AppData\Roaming\Code\User\settings.json -ItemType SymbolicLink -Value "$HOME\dotfiles\VS Code\settings.json"
